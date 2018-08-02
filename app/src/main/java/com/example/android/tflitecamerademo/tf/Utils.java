f4ce10b353ec5b966f6694554777d0625cccc949
Aldric ANDRE <aldric.andre@external.softbankrobotics.com>
2018-08-02 14:20:17 +0200

    remove unused import

--- app/src/main/java/com/example/android/tflitecamerademo/tf/Utils.java
+++ app/src/main/java/com/example/android/tflitecamerademo/tf/Utils.java
@@ -2,9 +2,6 @@ package com.example.android.tflitecamerademo.tf;
 
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 
 public class Utils {
 
