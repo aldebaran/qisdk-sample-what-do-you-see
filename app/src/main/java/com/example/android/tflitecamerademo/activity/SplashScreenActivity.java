249aee13f62e2d14b5508ef7ff1e3511835a9837
Aldric ANDRE <aldric.andre@external.softbankrobotics.com>
2018-08-29 15:16:04 +0200

    Delete immersive mode, RobotActivity do it

--- app/src/main/java/com/example/android/tflitecamerademo/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/android/tflitecamerademo/activity/SplashScreenActivity.java
@@ -39,18 +39,6 @@ public class SplashScreenActivity extends RobotActivity implements RobotLifecycl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /**
-         * Put the Activity in fullscreen mode
-         */
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
         setSpeechBarDisplayStrategy(SpeechBarDisplayStrategy.OVERLAY);
 
         setContentView(R.layout.activity_splashscreen);
